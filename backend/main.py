eb8bff4a913aeef7b149b8d095a37ee5324c8327
Gagan <gaganchandra079@gmail.com>
2026-09-03 00:24:39 +0530

    Fix broken CORS middleware syntax

--- backend/main.py
+++ backend/main.py
@@ -65,16 +65,7 @@ app = FastAPI(
     ),
     version="1.0.0"
 )
-_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5174",
-        "http://127.0.0.1:5174"
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
@@ -109,7 +100,7 @@ models = load_models()
 # HOME
 # ---------------------------------
 
-@app.get("/"app.add)
+@app.get("/")
 def home():
 
     return {
